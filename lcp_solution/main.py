0fdb95692bce384f4baa50dee1a702409552fb2a
Karen Pu <karen.c.pu@vanderbilt.edu>
2025-03-08 17:10:17 -0800

    completed lcp

---
+++ lcp_solution/main.py
@@ -0,0 +1,137 @@
+from tandem_transform import tandem_transform_lcs
+from utils import split_string
+from global_alignment import global_alignment
+from copy import deepcopy
+import sys
+
+def run_lcp(s: str, t: str, k1: int, k2: int, v: int, depth: int = 0, max_depth: int = 5, all_kmers=None):
+    """
+    Purpose:
+        Uses divide and conquer to recursively run tandem_transform_lcs
+    
+    Args:
+        s: String s
+        t: String t
+        k1: int (such that k1 < k2 -- see tandem_transform_lcs)
+        k2: int (longest possible kmer length)
+        v: similarity threshold 
+        depth: Current recursion depth
+        max_depth: maximum recursion depth to prevent infinite recursion
+        all_kmers: List of kmers used 
+        
+    Returns:
+        final_score: final alignment score
+        final_s: final s aligned string
+        final_t: final t aligned string
+        all_kmers: all kmers used in transformation
+    """
+
+    if all_kmers is None:
+        all_kmers = []
+    
+    # Base case: max recursion depth reached
+    if depth >= max_depth:
+        print("No tandem transformation possible.")
+        return
+
+    # run initial tandem transform 
+    result = tandem_transform_lcs(s, t, k1, k2, v)
+
+    # Update kmers_used
+    if result[3]:
+        all_kmers.extend(result[3])
+
+    # base case: s and t are past similarity threshold
+    if result[0] > v:
+        return result[0], result[1], result[2], all_kmers
+    
+    # second base case: no possible tandem transformations, as s and t below smallest possible kmer transformation
+    if len(s) < k1 and len(t) < k1:
+        return result[0], result[1], result[2], all_kmers
+    
+    # third base case: no common shared subsequence
+    lus = result[-1]
+    if not lus:
+        return result[0], result[1], result[2], all_kmers
+    
+    # begin divide and conquer approach
+    s = result[1].replace("-","")
+    t = result[2].replace("-","")
+    s_left, s_right, t_left, t_right = split_string(s,t,lus)
+
+    # keep track of scores
+    final_score = result[0] 
+    final_s = result[1]
+    final_t = result[2]
+    final_kmers = deepcopy(all_kmers)
+
+    # recursively call for left side
+    left_side = None
+    if s_left and t_left and (len(s_left) >= k1 or len(t_left) >= k1):
+
+        left_side = run_lcp(s_left, t_left, k1, k2, v, depth + 1, max_depth, deepcopy(all_kmers))
+        s_trans = left_side[1].replace("-", "") + lus + s_right
+        t_trans = left_side[2].replace("-", "") + lus + t_right
+
+        # did left transform result in an optimal global alignment score?
+        left_score, ls, lt = global_alignment(s_trans, t_trans)
+            
+        if left_score > final_score:
+            final_score = left_score
+            final_s = ls
+            final_t = lt
+            final_kmers = left_side[3]
+
+    # recursively call for right side
+    right_side = None
+    if s_right and t_right and (len(s_right) >= k1 or len(t_right) >= k1):
+        right_side = run_lcp(s_right, t_right, k1, k2, v, depth + 1, max_depth, deepcopy(all_kmers))
+
+        s_trans = s_left + lus + right_side[1].replace("-", "")
+        t_trans = t_left + lus + right_side[2].replace("-", "")
+            
+        # did right transform result in a optimal global alignment score?
+        right_score, rs, rt = global_alignment(s_trans, t_trans)
+            
+        if right_score > final_score:
+            final_score = right_score
+            final_s = rs
+            final_t = rt
+            final_kmers = right_side[3]
+
+    
+    return final_score, final_s, final_t, final_kmers
+
+
+if __name__ == "__main__":
+    # which test case do you want to use?
+    i = 2
+    homedir = ""
+
+    with open(f"{homedir}/test_cases/test_{i}.txt", "r") as f:
+        inputs = f.readlines()
+
+    print(f"Test case description:\n {inputs[0]}")
+
+    s = inputs[1].replace("\n","")
+    t = inputs[2].replace("\n","")
+    k1 = inputs[3].replace("\n","")
+    k2 = inputs[4].replace("\n","")
+    v  = inputs[5].replace("\n","")
+    final_score, s_final, t_final, kmers_final = run_lcp(s, t, int(k1), int(k2), int(v))
+
+    # no possible tandem transformation if similarity threshold isn't reached at this point
+    if final_score < int(v):
+        print("Couldn't find a good tandem transformation, but here's the best attempt:\n")
+        print(f"Final Score: {final_score}")
+        print(f"Original Strings: \n{s}\n{t}")
+        print(f"Aligned Transformation:\n{s_final}\n{t_final}")
+        print(f"Kmers Used: {kmers_final}")
+        print("=====================================\n")
+    else:
+        print("\n===== LCP Transformation Results =====")
+        print(f"Final Score: {final_score}")
+        print(f"Original Strings: \n{s}\n{t}")
+        print(f"Aligned Transformation:\n{s_final}\n{t_final}")
+        print(f"Kmers Used: {kmers_final}")
+        print("=====================================\n")
---
+++ lcp_solution/tandem_transform.py
@@ -0,0 +1,230 @@
+from global_alignment import global_alignment
+from utils import get_search_space_left, get_search_space_right, split_string
+import re
+
+
+def lcp_array(comb, suffix_a):
+    """
+    Purpose:
+        generates the lcp array
+
+    Args:
+        comb (str): combination of both strings with "$" appended to the  (s + "$" + t + "#")
+        suffix_a (array): suffix array 
+
+        suffix array 
+        - all the suffixes in sorted order with the index in which they occur 
+
+    Returns:
+        array: the lcp array 
+    """
+
+    rank = [0] * len(comb)
+    lcp = [0] * len(comb)
+    
+    for i, suffix in enumerate(suffix_a):
+        rank[suffix] = i
+    
+    idx = 0
+    for i in range(len(comb)):
+        if rank[i] > 0:
+            j = suffix_a[rank[i] - 1]
+            while (i + idx < len(comb)) and (j + idx < len(comb)) and (comb[i + idx] == comb[j + idx]):
+                idx += 1
+            lcp[rank[i]] = idx
+            if idx > 0:
+                idx -= 1
+    return lcp
+
+
+def lus(s, t, k1):
+    """
+    Purpose:
+        find optimal unique common shared subsequence 
+        only returns one substring 
+    Args:
+        s: string S 
+        t: string T
+        k1: int (such that k1 < k2 -- see tandem_transform_lcs)
+
+    Returns: 
+        the longest common unique subsequence between strings S and T. 
+        if there are two longest common subsequences of the same length, 
+        return the one which maximizes the global alignment score.
+        
+        our code breaks if there are no matches between S and T 
+        
+    """
+
+    # find longest unique substrings between s and t
+    comb = s + "$" + t + "#"
+
+    # craete suffix array
+    suffixes = [(comb[i:], i) for i in range(len(comb))]
+    suffixes.sort()
+    suffix_array = [suffix[1] for suffix in suffixes]
+    
+    # create lcp array
+    lcp = lcp_array(comb, suffix_array)
+    
+    # find all unique substrings
+    all_shared_substrings = []
+
+    for i in range(1, len(comb)):
+        suff_one = suffix_array[i]
+        suff_two = suffix_array[i - 1]
+
+        if (suff_one < len(s) and suff_two > len(s)) or (suff_one > len(s) and suff_two < len(s)):
+            if lcp[i] > 0:
+                if (i-1) >= 0:
+                     prev = lcp[i - 1] 
+                else:
+                     prev = 0
+                if (i+1) < len(lcp):
+                     next = lcp[i+1]
+                else:
+                     next = 0
+
+                if lcp[i] > max(prev, next):
+                    shared = comb[suff_one : suff_one + lcp[i]]
+                    all_shared_substrings.append(shared)
+    
+    # find longest string with most optimal score
+    max_len = 0
+    for string in all_shared_substrings:
+        max_len = max(len(string), max_len)
+
+    longest = None
+    cur_score = float("-inf")
+
+    for string in all_shared_substrings:
+        if len(string) == max_len and len(string) > k1:
+            sl, sr, tl, tr = split_string(s, t, string)
+            score = global_alignment(sl, tl)[0] + global_alignment(sr, tr)[0]
+            if score > cur_score:
+                longest = string
+    return longest
+
+
+def tandem_transform_lcs(s: str, t: str, k1: int, k2: int, v: int):
+        """
+        Purpose:
+                Finds longest unique substring between s and t and greedily applies most optimal tandem transformation
+                on left and right flank of LUS.
+        Args:
+                s: string S 
+                t: string T
+                k1: int (such that k1 < k2 -- see tandem_transform_lcs)
+                k2: int (longest possible kmer length)
+                v: similarity threshold
+        
+        Returns: 
+                score[0]: alignment score
+                score[1]: globally aligned s string
+                score[2]: globally aligned t string
+                kmers: kmers used in tandem transformaation
+                common_alignment: lus used
+        """
+    
+        # keep track of kmers
+        kmers = []
+        max_iters = 100
+        iters = 0
+
+        # This while loop ensures that all potential tandem transformations around alignment are explored
+        while iters < max_iters:
+
+                score = global_alignment(s, t)
+                if score[0] >= v:
+                        if iters == 0:
+                                print("Strings similar enough. No transformations needed")
+                        return (score[0], score[1], score[2], kmers, "")
+
+                # find best possible alignment between s and t longer than k1, uses greedy approach
+                common_alignment = lus(s, t, k1)
+                
+                # return if no common alignments exist
+                if not common_alignment:
+                        return (score[0], score[1], score[2], kmers, common_alignment)
+                
+                indices = [(x.start(), x.end() - 1) for x in re.finditer(re.escape(common_alignment), s)][0]
+
+        
+                # align s and t to common prefix, get the left flanking and right flanking sequence
+                sl, sr, tl, tr = split_string(s, t, common_alignment)
+
+                # left side applied transformations
+
+                # left kmer search space
+                search_space_l = get_search_space_left(sl, tl, k1, k2, common_alignment)
+
+                # test left side search space values and find the one that leads to highest global alignment score when applied
+                max_score = float("-inf")
+                target_kmer = None
+                transformation = ""
+                for transforms in search_space_l:
+                        if transforms[-1] == "del":
+                                s_trans = sl[:len(sl) - len(transforms[0])]
+                        else:
+                                s_trans = sl + transforms[0]
+                        reconstructed = s_trans + common_alignment  + s[indices[-1] + 1:]
+
+                        # apply transformation and check alignment score
+                        score = global_alignment(reconstructed, t)
+
+                        if score[0] > v:
+                                # if threshold is reached, a tandem transformation has been found
+                                kmers.append(transforms)
+                                return (score[0], score[1], score[2], kmers, common_alignment)
+                        else:
+                                # assign the highest scoring transformation 
+                                if score[0] > max_score:
+                                        transformation = reconstructed
+                                        target_kmer = transforms
+
+                # right kmer search space
+                if search_space_l:
+                        s = transformation
+                        kmers.append(target_kmer)
+                indices = [(x.start(), x.end() - 1) for x in re.finditer(re.escape(common_alignment), s)][0]
+                search_space_r = get_search_space_right(sr, tr, k1, k2, common_alignment)
+
+
+                # test right side search space values and select the one that leads to highest global alignment score when applied
+                transformation_r = ""
+                for transforms in search_space_r:
+
+                        if transforms[-1] == "del":
+                                s_trans = sr[len(transforms[0]):]
+                        else:
+                                s_trans = transforms[0] + sr
+        
+                        reconstructed = s[:indices[-1] + 1] + s_trans
+
+                        # apply transformation and check alignment score
+                        # score = similarity(reconstructed, t)
+                        score = global_alignment(reconstructed, t)
+
+                        if score[0] > v:
+                                # if threshold is reached, a tandem transformation has been found
+                                kmers.append(transforms)
+                                return (score[0], score[1], score[2], kmers, common_alignment)
+                        else:
+                                # assign the highest scoring transformation 
+                                if score[0] > max_score:
+                                        transformation_r = reconstructed
+                                        target_kmer = transforms
+
+                # transformation on right side complete, final s transformation for this iteration
+                if search_space_r:
+                        s = transformation_r
+                        kmers.append(target_kmer)
+
+                # no more possible tandem duplications or deletions
+                if not search_space_l and not search_space_r:
+                        #print("All tandem transformations exhausted for this alignment.")
+                        return (score[0], score[1], score[2], kmers, common_alignment)
+                
+                iters += 1
+                
+        return 
